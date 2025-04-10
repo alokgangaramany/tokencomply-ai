0baf2f455a72982c73a3c075ecbd6d943a640c39
alokgangaramany <71618840+alokgangaramany@users.noreply.github.com>
2025-04-09 22:22:46 -0500

    Update persona_api.py

--- persona_api.py
+++ persona_api.py
@@ -12,7 +12,7 @@ HEADERS = {
     "Content-Type": "application/json"
 }
 
-def generate_persona_flow_url(itmpl_id, name, email):
+def generate_persona_url(itmpl_id, name, email):
     # This URL uses the inquiry template to auto-create the inquiry when loaded
     return f"https://withpersona.com/verify?template-id={itmpl_id}&prefill[name]={name}&prefill[email]={email}"
 
